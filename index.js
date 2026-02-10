d527282333835e3a4c8c811e69aa739fa359debd
YashMayanil <yashmayanil87@tgmail.com>
2026-02-10 13:42:51 +0530

    Initial project upload

---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require("express")
+const path = require("path")
+const app=express ();
+const port=8000;
+const {v4:uuidv4}= require("uuid")
+const methodOverride = require("method-override")
+
+
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'))
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"))
+app.use(express.static(path.join(__dirname,"public")))
+
+let posts =[
+    {
+        id:uuidv4(),
+        username:"apnacollege",
+        content:"I love coding!"
+    },
+    {
+        id:uuidv4(),
+        username:"yashmayanil",
+        content:"getting started with mern"
+    },
+    {
+        id:uuidv4(),
+        username:"omprakash",
+        content:"Loving to watch criket"
+    },
+    {
+        id:uuidv4(),
+        username:"hello34",
+        content:"Just doing great"
+    },
+]
+
+
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{posts})
+})
+
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs")
+})
+
+app.post("/posts",(req,res)=>{
+    let {username,content}= req.body;
+    let id = uuidv4();
+    posts.push({id,username,content});
+    res.redirect("/posts")
+})
+
+app.get("/posts/:id",(req,res)=>{
+    let {id}= req.params;
+    let post = posts.find((p)=>id===p.id);
+    res.render("show.ejs",{post})
+})
+
+app.patch("/posts/:id",(req,res)=>{
+    let {id}= req.params;
+    let newContent= req.body.content;
+    let post= posts.find ((p)=>id===p.id)
+    post.content = newContent;
+    res.redirect("/posts")
+})
+
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id}= req.params;
+    let post= posts.find((p)=>id===p.id)
+    res.render("edit.ejs",{post})
+})
+
+app.delete("/posts/:id",(req,res)=>{
+     let {id}= req.params;
+    posts= posts.filter((p)=>id!==p.id)
+    res.redirect("/posts")
+    
+})
+
+app.listen(port,()=>{
+    console.log("Server is running...")
+})
